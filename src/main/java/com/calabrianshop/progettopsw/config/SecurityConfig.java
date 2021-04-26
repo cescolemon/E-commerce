91505dc174dcca3385c9372715bdef55248bc1c9
cescolemon <79991613+cescolemon@users.noreply.github.com>
2021-04-26 12:24:52 +0200

    new

--- src/main/java/com/calabrianshop/progettopsw/config/SecurityConfig.java
+++ src/main/java/com/calabrianshop/progettopsw/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return new CorsFilter(source);
     }
-    
+
 }
 
 
